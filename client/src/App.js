8961c81041a300e6021571ae4281084f53608d99
monte-dev <badziobw@gmail.com>
2023-06-27 02:17:55 +0100

    add client side task removal/adding

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,33 @@
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import shortId from 'shortid';
 const App = () => {
+	const [socket, setSocket] = useState(null);
+	const [tasks, setTasks] = useState([]);
+	const [taskName, setTaskName] = useState('');
+	const id = shortId();
+
+	useEffect(() => {
+		const socket = io('http://localhost:8000');
+		setSocket(socket);
+	}, []);
+
+	const removeTask = (taskId) => {
+		setTasks((tasks) => tasks.filter((task) => task.id !== taskId));
+	};
+
+	const submitForm = (e) => {
+		e.preventDefault();
+		const newTask = { name: taskName, id: id };
+		addTask(newTask);
+		socket.emit('addTask', newTask);
+		setTaskName('');
+	};
+
+	const addTask = (newTask) => {
+		setTasks((tasks) => [...tasks, newTask]);
+	};
+
 	return (
 		<div className="App">
 			<header>
@@ -7,25 +36,29 @@ const App = () => {
 
 			<section className="tasks-section" id="tasks-section">
 				<h2>Tasks</h2>
-
 				<ul className="tasks-section__list" id="tasks-list">
-					<li className="task">
-						Shopping{' '}
-						<button className="btn btn--red">Remove</button>
-					</li>
-					<li className="task">
-						Go out with a dog{' '}
-						<button className="btn btn--red">Remove</button>
-					</li>
+					{tasks.map((task) => (
+						<li key={task.id} className="task">
+							{task.name}
+							<button
+								className="btn btn--red"
+								onClick={() => removeTask(task.id)}
+							>
+								Remove
+							</button>
+						</li>
+					))}
 				</ul>
 
-				<form id="add-task-form">
+				<form id="add-task-form" onSubmit={submitForm}>
 					<input
 						className="text-input"
 						autocomplete="off"
 						type="text"
 						placeholder="Type your description"
 						id="task-name"
+						value={taskName}
+						onChange={(event) => setTaskName(event.target.value)}
 					/>
 					<button className="btn" type="submit">
 						Add
