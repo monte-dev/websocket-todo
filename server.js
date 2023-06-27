76e6e0faacd5b1391c7532b0804eb964f16d5b67
monte-dev <badziobw@gmail.com>
2023-06-27 01:55:59 +0100

    add server socket connections and add/remove task emitters

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
-
 const app = express();
+
+const tasks = [];
 
 const server = app.listen(process.env.PORT || 8000, () => {
 	console.log('Server is running...');
+});
+const io = socket(server);
+
+io.on('connection', (socket) => {
+	socket.emit('updateData', { tasks: tasks });
+
+	socket.on('addTask', (task) => {
+		tasks.push({ name: task.name, id: task.id });
+		socket.broadcast.emit('addTask', { name: task.name, id: task.id });
+	});
+
+	socket.on('removeTask', (id) => {
+		const taskIndex = tasks.findIndex((task) => task.id === id);
+		if (taskIndex !== -1) {
+			tasks.splice(taskIndex, 1);
+		}
+		socket.broadcast.emit('addTask', {
+			name: task.name,
+			id: task.id,
+		});
+	});
 });
 
 app.use((req, res) => {
